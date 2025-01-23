8fe19b8cc8578b62437645c6e5e46903d748069c
abhishek <abhishekjaswal1122@gmail.com>
2025-01-23 19:28:29 +0530

    add some files

---
+++ scripts.js
@@ -0,0 +1,40 @@
+
+// let step1 = document.getElementById("step1");
+// let step2 = document.getElementById("step2");
+// let ran = Math.floor(Math.random() * 50)+1 ;
+// let show = document.getElementById("show");
+// let count =0;
+
+// function guess(){
+//   console.log(ran)
+//   count++; 
+//   let userName = document.getElementById("name").value;
+//       document.querySelector("h2").innerHTML = "Hello "+userName ;
+//       let check = document.getElementById("number").value;
+//       if(check == ran){
+//          show.innerHTML = "Congratulations! You have guessed the correct number!" + count + " guesses";
+//       }
+//       else if(check > ran){
+//            show.innerHTML = "You have guessed too high. Try again!" + count + " guesses";
+//       }
+//       else if(check < ran){
+//             show.innerHTML = "You have guessed too low. Try again!" + count + " guesses";
+//       }
+//       else{
+//             show.innerHTML = "Please enter a number!";
+//       } 
+      
+//       if(count>5){
+//       alert("You have exceed   guesses");
+//        }
+//       }
+   
+    
+// function send(){
+//   step1.style.display = "none";
+//   step2.style.display = "block";
+//   let userName = document.getElementById("name").value;
+//   document.querySelector("h2").innerHTML = "Hello "+ userName ;
+// }
+// step1.style.display = "block";
+// step2.style.display = "none";
